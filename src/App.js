053fa3eba41b2261bc81477dd2978988e9a2faf3
samiTab <sami.tabibel@gmail.com>
2022-02-05 15:15:37 +0100

    minor

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ahla!
+          Ahla from aws EBS!
         </p>
         <a
           className="App-link"
